cce9bbf10ef4a152d31c710a03b674ef47f04787
HongyanJi <38867283+HongyanJi@users.noreply.github.com>
2020-07-30 23:17:16 -0500

    delete comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,4 @@
 /*
- * Hongyan Ji's Homework assignment
- */
-
-/*
  * The main function of this program, which calls the constructor of Game class and start and play the game
  */
 public class TicTacToe {
